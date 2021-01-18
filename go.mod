a78b50dfafb23ac62a52179cd3cec5021c4712c3
Geßner, Simon <gessnersn@gmail.com>
2021-01-18 20:31:23 +0100

    basic imperfect image generation functionality

--- go.mod
+++ go.mod
@@ -3,9 +3,15 @@ module imagen
 go 1.15
 
 require (
+	github.com/Knetic/govaluate v3.0.0+incompatible
+	github.com/Maldris/mathparse v0.0.0-20170508133428-f0d009a7a773
+	github.com/apaxa-go/eval v0.0.0-20171223182326-1d18b251d679 // indirect
+	github.com/apaxa-go/helper v0.0.0-20180607175117-61d31b1c31c3 // indirect
 	github.com/howeyc/fsnotify v0.9.0 // indirect
 	github.com/joho/godotenv v1.3.0
 	github.com/mattn/go-colorable v0.1.8 // indirect
 	github.com/pilu/config v0.0.0-20131214182432-3eb99e6c0b9a // indirect
 	github.com/pilu/fresh v0.0.0-20190826141211-0fa698148017 // indirect
+	github.com/zdebeer99/goexpression v0.0.0-20200529021658-821357644f79 // indirect
+	golang.org/x/text v0.3.5 // indirect
 )
--- imagen.go
+++ imagen.go
@@ -3,10 +3,16 @@ package main
 import (
 	"fmt"
 	"html/template"
+	"image"
+	"image/color"
+	"image/png"
 	"log"
 	"net/http"
 	"os"
+	"strconv"
+	"strings"
 
+	"github.com/zdebeer99/goexpression"
 	"github.com/joho/godotenv"
 )
 
@@ -21,7 +27,7 @@ func main() {
 
 	mux.HandleFunc("/", indexHandler)
 	mux.HandleFunc("/imagen/", imagenHandler)
-	log.Fatal(http.ListenAndServe(":" + port, mux))
+	log.Fatal(http.ListenAndServe(":"+port, mux))
 }
 
 func getPortFromEnv() string {
@@ -45,18 +51,94 @@ func indexHandler(writer http.ResponseWriter, request *http.Request) {
 }
 
 func imagenHandler(writer http.ResponseWriter, request *http.Request) {
+	special := "form input:\n"
+	formError := request.ParseForm()
+	check(formError)
+	width, widthExists := request.Form["width"]
+	height, heightExists := request.Form["height"]
+	red, redExists := request.Form["r"]
+	green, greenExists := request.Form["g"]
+	blue, blueExists := request.Form["b"]
+
+	imagePath := ""
+	if widthExists && heightExists && redExists && greenExists && blueExists {
+		imagePath = computeImage(width, height, red, green, blue)
+		imagePath = "/assets/outimage.png"
+	}
+
+	for key, value := range request.Form {
+		special += fmt.Sprintf("%v: %v\n", key, value)
+	}
 	data := struct {
 		Title string
 		Image string
 	}{
 		Title: "Imagen - Image Generator",
-		Image: "/assets/black640x480.jpg",
+		Image: imagePath,
 	}
-	err := baseTpl.Execute(writer, data)
-	check(err)
+	templateError := baseTpl.Execute(writer, data)
+	check(templateError)
 }
 
-func check (err error) {
+func computeImage(width []string, height []string, red []string, green []string, blue []string) string {
+	outPath := "assets/outimage.png"
+
+	widthString := strings.Join(width, "")
+	widthInt, _ := strconv.Atoi(widthString)
+	heightString := strings.Join(height, "")
+	heightInt, _ := strconv.Atoi(heightString)
+	redString := strings.Join(red, "")
+	greenString := strings.Join(green, "")
+	blueString := strings.Join(blue, "")
+
+	newImage := Image{
+		widthInt, heightInt, redString, greenString, blueString,
+	}
+	f, err := os.Create(outPath)
+	check(err)
+	defer f.Close()
+	err = png.Encode(f, newImage)
+	check(err)
+	return outPath
+}
+
+type Image struct {
+	width  int
+	height int
+	red    string
+	green  string
+	blue   string
+}
+
+func (i Image) ColorModel() color.Model {
+	return color.RGBAModel
+}
+
+func (i Image) Bounds() image.Rectangle {
+	return image.Rect(0, 0, i.width, i.height)
+}
+
+func (i Image) At(x, y int) color.Color {
+	redExpression := i.red
+	greenExpression := i.green
+	blueExpression := i.blue
+
+	// unfortunately "github.com/zdebeer99/goexpression" does not support ^ (exponent) expression
+	context := map[string]interface{}{
+		"x": x,
+		"y": y,
+	}
+
+	var redValue, greenValue, blueValue float64
+	redValue = goexpression.Eval(redExpression, context)
+	greenValue = goexpression.Eval(greenExpression, context)
+	blueValue = goexpression.Eval(blueExpression, context)
+
+	col := color.RGBA{uint8(redValue), uint8(greenValue), uint8(blueValue), 255}
+	return col
+}
+
+func check(err error) {
 	if err != nil {
 		log.Fatal(err)
 	}
